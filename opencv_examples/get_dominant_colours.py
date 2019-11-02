618d8b675cc127bdf4f8f07a3efd3cc6ba9c823d
Andrey <andrey.markin.ppc@gmail.com>
2019-11-03 02:06:10 +0300

    Add support for directories

--- opencv_examples/get_dominant_colours.py
+++ opencv_examples/get_dominant_colours.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import pandas as pd
 import webcolors
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", required = True)
@@ -28,22 +29,26 @@ def get_dominant_colours(path, threshold = 0.001, names = False):
 
     # define data frame
     df = pd.DataFrame(image, columns = ['b', 'g', 'r'])
-
     # calculate proportions
     df = df.groupby(['r','g', 'b']).size().sort_values(ascending=False)\
             .reset_index(name='count').drop_duplicates()
     df["prop"] = df["count"] / sum(df["count"])
     df = df.query("prop > %f" %(threshold))
-    # if names:
-        # df["colour"] = [get_colour_name([c[0], c[1], c[2]]) for c in df[["r", "g", "b"]].values]
-        # df = df.groupby(["colour"])["prop"].sum()\
-        #         .sort_values(ascending=False)\
-        #         .reset_index(name="value")
-        # df["prop"] = df["value"] / sum(df["value"])
+    df = df.drop('count', axis = 1)
+    df["ytid"] = os.path.dirname(path).split("/")[-1]
+    df["frame"] = os.path.basename(path).split("_")[-1].split(".")[0]
     return df
 
 
 # colours = get_dominant_colours("videos/sample_video/frame_13.jpg", threshold=0.0001)
-colours = get_dominant_colours(args.file, args.threshold)
-print(colours)
+path = args.file
+if os.path.isfile(path):
+    colours = get_dominant_colours(args.file, args.threshold)
+    colours.to_csv(path.split(".")[0] + ".csv", index = False)
+elif os.path.isdir(path):
+    print("running on a folder...")
+    files = os.listdir(path)
+    colours = [get_dominant_colours(os.path.join(path, file), args.threshold) for file in files]
+    pd.concat(colours).to_csv(path + "/colours.csv", index = False)
+
 # [get_colour_name([c.r[0], c.g[0], c.g[0]]) for c in colours]
