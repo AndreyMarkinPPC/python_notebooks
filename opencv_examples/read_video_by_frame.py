d10141d2fdb1a546f32fa64307fde6654647fd99
Andrey <andrey.markin.ppc@gmail.com>
2019-11-02 16:06:35 +0300

    Add saving to to separate directories based on file

--- opencv_examples/read_video_by_frame.py
+++ opencv_examples/read_video_by_frame.py
@@ -1,23 +1,42 @@
 import cv2
 import youtube_dl
 import pafy
+import argparse
+import pandas as pd
+import os
 
-url = 'https://youtu.be/feRrKiZRw04'
-vPafy = pafy.new(url)
-play = vPafy.getbest(preftype="webm")
-cap = cv2.VideoCapture(play.url)
-seconds = 1
-fps = cap.get(cv2.CAP_PROP_FPS) # Gets the frames per second
-multiplier = fps * seconds
-print(multiplier)
-print(cap.get(cv2.CAP_PROP_FPS))
-ret = True
-while (ret):
-    frameId = int(round(cap.get(1)))
-    ret,frame = cap.read()
-    if frameId % multiplier == 0:
-        # print("saving...")
-        if frameId / multiplier < 10:
-            cv2.imwrite("videos/sample_video/frame_0%d.jpg" % int(frameId / multiplier) , frame)
+# parser = argparse.ArgumentParser()
+# 
+# parser.add_argument("-f", "--input-file", required = True)
+# args = parser.parse_args()
+# print(args)
+if True:
+    data = pd.read_csv("videos/input/videos.csv")
+    for video in data["ytid"]:
+        url = 'https://youtu.be/%s' %(video)
+        if os.path.isdir("videos/%s/" %(video)):
+            print("%s is already fetched, skipping..." %(video))
         else:
-            cv2.imwrite("videos/sample_video/frame_%d.jpg" % int(frameId / multiplier) , frame)
+            try:
+                vPafy = pafy.new(url)
+                play = vPafy.getbest(preftype="webm")
+                cap = cv2.VideoCapture(play.url)
+                seconds = 1
+                fps = cap.get(cv2.CAP_PROP_FPS) # Gets the frames per second
+                multiplier = fps * seconds
+                # print(multiplier)
+                # print(cap.get(cv2.CAP_PROP_FPS))
+                ret = True
+                print("running for video %s" %(video))
+                os.mkdir("videos/%s/" %(video))
+                while (ret):
+                    frameId = int(round(cap.get(1)))
+                    ret,frame = cap.read()
+                    if frameId % multiplier == 0:
+                        # print("saving...")
+                        if frameId / multiplier < 10:
+                            cv2.imwrite("videos/%s/frame_0%d.jpg" % (str(video), int(frameId / multiplier)), frame)
+                        else:
+                            cv2.imwrite("videos/%s/frame_%d.jpg" % (str(video), int(frameId / multiplier)), frame)
+            except OSError as error:
+                print(error)
