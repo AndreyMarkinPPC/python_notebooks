e328cd0939aec5c96e92d39d8d6164dfd2e5e5df
Andrey <andrey.markin.ppc@gmail.com>
2019-11-07 01:22:06 +0300

    Add integer rounding to fps

--- opencv_examples/read_video_by_frame.py
+++ opencv_examples/read_video_by_frame.py
@@ -14,18 +14,22 @@ if True:
     data = pd.read_csv("videos/input/videos.csv")
     for video in data["ytid"]:
         url = 'https://youtu.be/%s' %(video)
+        # print(len(os.listdir("videos/%s/" %(video))) > 1)
+        # TODO: Add check for folder with no images downloaded
         if os.path.isdir("videos/%s/" %(video)):
             print("%s is already fetched, skipping..." %(video))
         else:
             try:
+                print("trying video %s..." %(video))
                 vPafy = pafy.new(url)
                 play = vPafy.getbest(preftype="webm")
                 cap = cv2.VideoCapture(play.url)
                 seconds = 1
                 fps = cap.get(cv2.CAP_PROP_FPS) # Gets the frames per second
-                multiplier = fps * seconds
-                # print(multiplier)
-                # print(cap.get(cv2.CAP_PROP_FPS))
+                # print("fps: ", fps)
+                multiplier = int(fps * seconds)
+                # print("multiplier:", multiplier)
+                # print("fps :", cap.get(cv2.CAP_PROP_FPS))
                 ret = True
                 print("running for video %s" %(video))
                 os.mkdir("videos/%s/" %(video))
@@ -33,8 +37,9 @@ if True:
                     frameId = int(round(cap.get(1)))
                     ret,frame = cap.read()
                     if frameId % multiplier == 0:
-                        # print("saving...")
+                        # print("saving...", int(frameId / multiplier))
                         if frameId / multiplier < 10:
+                            # TODO: skipp saving empty files (0 byte length)
                             cv2.imwrite("videos/%s/frame_0%d.jpg" % (str(video), int(frameId / multiplier)), frame)
                         else:
                             cv2.imwrite("videos/%s/frame_%d.jpg" % (str(video), int(frameId / multiplier)), frame)
